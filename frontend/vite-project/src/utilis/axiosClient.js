15e31b8bdd34807ff4b27aaa3fdc85ed99ad868d
Priya-123-debug <lakhisarai9155@gmail.com>
2026-01-04 01:11:10 +0530

    something baseurl changed

--- frontend/vite-project/src/utilis/axiosClient.js
+++ frontend/vite-project/src/utilis/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosClient = axios.create({
-  baseURL: Process.env.VITE_BACKEND_URL|| "https://codingplatform-oihe.onrender.com",
+ baseURL: import.meta.env.VITE_BACKEND_URL || "https://codingplatform-oihe.onrender.com",
   withCredentials: true, // i told browser that attach cookie with it
   headers: {
     "Content-Type": "application/json",
